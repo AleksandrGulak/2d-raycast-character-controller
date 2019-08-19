855204f56343ed72c13d6dc26896113c00d48127
ScoobySnacks69 <43790975+ScoobySnacks69@users.noreply.github.com>
2019-08-19 16:18:38 +0300

    Камит

---
+++ 2d-raycast-character-controller/Assets/Scripts/Controller2d/PlatformController.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : RaycastController
+{
+    public LayerMask PassengerMask;
+    public Vector3 Move;
+    public Vector3[] LocalWaypoints;
+    public float Speed;
+    public bool Cyclic;
+    public float WaitTime;
+    [Range(0,2)]
+    public float EaseAmount;
+
+    float nextMoveTime;
+    int fromWaypointIndex;
+    float percentBetweenWaypoints;
+    Vector3[] globalWaypoints;
+    List<PassengerMovement> passengerMovement;
+    Dictionary<Transform, Controller> passengerDictionary = new Dictionary<Transform, Controller>();
+    public override void Start()
+    {
+        base.Start();
+        globalWaypoints = new Vector3[LocalWaypoints.Length];
+        for (int i = 0; i < LocalWaypoints.Length; i++)
+        {
+            globalWaypoints[i] = LocalWaypoints[i] + transform.position;
+        }
+    }
+
+    void Update()
+    {
+        UpdateRaycastOrigins();
+        Vector3 velocity = CalculatePlatformMovement() ;
+        CalculactePassengerMovement(velocity);
+        MovePassengers(true);
+        transform.Translate(velocity);
+        MovePassengers(false);
+
+    }
+    float Ease(float x)
+    {
+        float a = EaseAmount +1;
+        return Mathf.Pow(x, a) / (Mathf.Pow(1-x,a));
+    }
+
+    Vector3 CalculatePlatformMovement()
+    {
+        if(Time.time < nextMoveTime)
+        {
+            return Vector3.zero;
+        }
+        fromWaypointIndex %= globalWaypoints.Length;
+        int toWaypointIndex = (fromWaypointIndex + 1) % globalWaypoints.Length;
+        float distanceBetweenWaypoints = Vector3.Distance(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex]);
+        percentBetweenWaypoints += Time.deltaTime * Speed / distanceBetweenWaypoints;
+        percentBetweenWaypoints = Mathf.Clamp01(percentBetweenWaypoints);
+        float easedPercentBetweenWaypoints = Ease(percentBetweenWaypoints);
+        Vector3 newPos = Vector3.Lerp(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex], easedPercentBetweenWaypoints);
+        if (percentBetweenWaypoints >= 1)
+        {
+            percentBetweenWaypoints = 0;
+            fromWaypointIndex ++;
+            if (!Cyclic)
+            {
+                if (fromWaypointIndex >= globalWaypoints.Length - 1)
+                {
+                    fromWaypointIndex = 0;
+                    System.Array.Reverse(globalWaypoints);
+                }
+            }
+            nextMoveTime = Time.time + WaitTime;
+        }
+            return newPos - transform.position;
+    }
+
+    void MovePassengers(bool beforeMovePlatform)
+    {
+        foreach (PassengerMovement passenger in passengerMovement)
+        {
+            if (!passengerDictionary.ContainsKey(passenger.transform))
+            {
+                passengerDictionary.Add(passenger.transform, passenger.transform.GetComponent<Controller>());
+            }
+
+            if (passenger.moveBeforePlatform == beforeMovePlatform)
+            {
+                passengerDictionary[passenger.transform].Move(passenger.velocity, passenger.standingOnPlatform);
+            }
+        }
+    }
+    void CalculactePassengerMovement(Vector3 velocity)
+    {
+        HashSet<Transform> movedPassengers = new HashSet<Transform>();
+        passengerMovement = new List<PassengerMovement>();
+        float directionX = Mathf.Sign(velocity.x);
+        float directionY = Mathf.Sign(velocity.y);
+        //Vertically moving platform
+        if (velocity.y != 0)
+        {
+            float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+            for (int i = 0; i < VerticalRayCount; i++)
+            {
+                Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+                rayOrigin += Vector2.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, PassengerMask);
+                if (hit && hit.distance !=0)
+                {
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = (directionY == 1) ? velocity.x : 0;
+                        float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
+                        passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), directionY == 1, true));
+                    }
+                }
+            }
+        }
+        //Horizontally moving platform
+        if (velocity.x != 0)
+        {
+            float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+            for (int i = 0; i < HorizontalRayCount; i++)
+            {
+                Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+                rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, PassengerMask);
+                if (hit && hit.distance != 0)
+                {
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
+                        float pushY = -skinWidth;
+                        passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), false, true));
+                    }
+                }
+            }
+        }
+        // Passenger on top of a horizontally or downward moving platform
+        if (directionY == -1 || velocity.y == 0 && velocity.x != 0)
+        {
+            float rayLength = skinWidth * 2;
+
+            for (int i = 0; i < VerticalRayCount; i++)
+            {
+                Vector2 rayOrigin = raycastOrigins.topLeft + Vector2.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, PassengerMask);
+                if (hit && hit.distance != 0)
+                {
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = velocity.x;
+                        float pushY = velocity.y;
+                        passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), true, false));
+                    }
+                }
+            }
+        }
+    }
+    struct PassengerMovement
+    {
+        public Transform transform;
+        public Vector3 velocity;
+        public bool standingOnPlatform;
+        public bool moveBeforePlatform;
+        public PassengerMovement(Transform _transform, Vector3 _velocity, bool _standingOnPlatform, bool _moveBeforePlatform)
+        {
+            transform = _transform;
+            velocity = _velocity;
+            standingOnPlatform = _standingOnPlatform;
+            moveBeforePlatform = _moveBeforePlatform;
+        }
+    }
+    void OnDrawGizmos()
+    {
+        if (LocalWaypoints != null)
+        {
+            Gizmos.color = Color.red;
+            float size = .3f;
+            for (int i = 0; i < LocalWaypoints.Length; i++)
+            {
+                Vector3 globalWaypointPos = (Application.isPlaying)?globalWaypoints[i]:LocalWaypoints[i] + transform.position;
+                Gizmos.DrawLine(globalWaypointPos - Vector3.up * size, globalWaypointPos + Vector3.up * size);
+                Gizmos.DrawLine(globalWaypointPos - Vector3.left * size, globalWaypointPos + Vector3.left * size);
+            }
+        }
+    }
+}
---
+++ 2d-raycast-character-controller/Assets/Scripts/Controller2d/Player.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+public class Player : MonoBehaviour
+{
+    [Range(0, 50)]
+    public float MoveSpeed = 6;
+    [Range(0, 1)]
+    public float accelerationTimeAirborne = .2f;
+    [Range(0, 1)]
+    public float accelerationTimeGrounded = .1f;
+    [Range(0, 50)]
+    public float MaxJumpHeight = 4;
+    [Range(0, 50)]
+    public float minJumpHeight = 1;
+    [Range(0, 10)]
+    public float TimeToJumpApex = .4f;
+    [Range(0, 4)]
+    public int ExtraJumps = 1;
+    [Range(2, 4)]
+    public float fallMultiplier = 2.5f;
+    public bool VariableJumpHeight;
+    [Range(0, 10)]
+    public float wallSlideSpeedMax = 3;
+    public float wallStickTime = .25f;
+    public Vector2 wallJumpClimb;
+    public Vector2 wallJumpOff;
+    public Vector2 wallLeap;
+
+    float timeToWallUnstick;
+    int jumps;
+    float maxJumpVelocity;
+    float minJumpVelocity;
+    float gravity;
+    float velocityXSmoothing;
+    Vector3 velocity;
+    Controller controller;
+    Vector2 directionalInput;
+    bool wallSliding;
+    int wallDirX;
+
+    void Start()
+    {
+        controller = GetComponent<Controller>();
+        gravity = -(2 * MaxJumpHeight) / Mathf.Pow(TimeToJumpApex, 2);
+        maxJumpVelocity = Mathf.Abs(gravity) * TimeToJumpApex;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+        jumps = ExtraJumps;
+        print("Gravity: " + gravity + "Max Jump Velocity " + maxJumpVelocity);
+    }
+
+    void FixedUpdate()
+    {
+        CalculateVelocity();
+        HandleWallSliding();
+
+        wallDirX = (controller.collisions.left) ? -1 : 1;
+
+        if (controller.collisions.below || controller.collisions.left || controller.collisions.right)
+        {
+            jumps = ExtraJumps;
+        }
+
+        controller.Move(velocity * Time.deltaTime, directionalInput);
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+    }
+
+    public void SetDirectionalInput(Vector2 input)
+    {
+        directionalInput = input;
+    }
+
+    public void OnJumpInputDown()
+    {
+        if (wallSliding)
+        {
+            if (wallDirX == directionalInput.x)
+            {
+                velocity.x = -wallDirX * wallJumpClimb.x;
+                velocity.y = wallJumpClimb.y;
+            }
+            else if (directionalInput.x == 0)
+            {
+                velocity.x = -wallDirX * wallJumpOff.x;
+                velocity.y = wallJumpOff.y;
+            }
+            else
+            {
+                velocity.x = -wallDirX * wallLeap.x;
+                velocity.y = wallLeap.y;
+            }
+        }
+        if (controller.collisions.below)
+        {
+            if (directionalInput.y != -1)
+            {
+                velocity.y = maxJumpVelocity;
+            }
+        }
+        if (jumps > 0 && directionalInput.y != -1)
+        {
+            velocity.y = maxJumpVelocity;
+            jumps--;
+        }
+    }
+
+    public void OnJumpInputUp()
+    {
+        if (velocity.y > minJumpVelocity)
+        {
+            velocity.y = minJumpVelocity;
+        }
+    }
+
+
+    void HandleWallSliding()
+    {
+        wallSliding = false;
+        if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
+        {
+            wallSliding = true;
+            if (velocity.y < -wallSlideSpeedMax)
+            {
+                velocity.y = -wallSlideSpeedMax;
+            }
+            if (timeToWallUnstick > 0)
+            {
+                velocityXSmoothing = 0;
+                velocity.x = 0;
+                if (directionalInput.x != wallDirX && directionalInput.x != 0)
+                {
+                    timeToWallUnstick -= Time.deltaTime;
+                }
+                else
+                {
+                    timeToWallUnstick -= wallStickTime;
+                }
+            }
+            else
+            {
+                timeToWallUnstick = wallStickTime;
+            }
+        }
+    }
+
+    void CalculateVelocity()
+    {
+        float targetVelocityX = directionalInput.x * MoveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+        if (velocity.y < 0)
+        {
+            velocity += Vector3.up * gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
